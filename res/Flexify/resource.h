f7d2427e37aafea7d8e16c70f4581f00b509b512
Aleksei <gav503244@gmail.com>
2019-07-28 02:07:20 +0300

    v0.0.2

---
+++ res/Flexify/resource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Flexify.rc
+//
+#define IDC_MYICON                      2
+#define IDD_FLEXIFY_DIALOG              102
+#define IDD_CHANGE_LOADMAP_DLG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_FLEXIFY                     107
+#define IDI_SMALL                       108
+#define IDC_FLEXIFY                     109
+#define IDR_MAINFRAME                   128
+#define IDD_RESTORE_LOADMAP_DLG         129
+#define IDD_SETTINGS_DLG                131
+#define IDC_RESTORE_LOADMAP_DLG_COMBO   1003
+#define IDC_CHANGE_LOADMAP_DLG_BUTTON_PATH 1006
+#define IDC_CHANGE_LOADMAP_DLG_STATIC_PATH 1007
+#define IDC_CHANGE_LOADMAP_DLG_STATIC_ERROR 1008
+#define IDD_CHANGE_LOADMAP_BUTTON_CANCEL 1009
+#define IDD_CHANGE_LOADMAP_BUTTON_OK    1010
+#define IDC_SETTINGS_DLG_BUTTON         1011
+#define IDC_CHANGE_LOADMAP_DLG_COMBO    1013
+#define IDC_SETTINGS_DLG_STATIC         1014
+#define ID_FILE_CSGO                    32771
+#define ID_FILE_                        32772
+#define ID_CSGO_BACKGRA                 32773
+#define ID_CSGO_MAPLOAD                 32774
+#define ID_MAPLOAD_ADD                  32775
+#define ID_MAPLOAD_CHANGE               32776
+#define ID_Menu                         32777
+#define ID_MAPLOAD_RESTORE              32778
+#define ID_FILE_SETTINGS                32779
+#define ID_BACKGROUND_ADD               32780
+#define ID_BACKGROUND_CHANGE            32781
+#define ID_BACKGROUND_SHOW              32782
+#define ID_BACKGROUND_RESTORE           32783
+#define ID_FILE_CONVERTER               32784
+#define ID_TOOLS_CONVERTER              32785
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32786
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
